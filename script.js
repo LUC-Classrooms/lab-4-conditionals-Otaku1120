0e5bf52d1acbbdfd8ab9fd4f243ab9c7e2b9b3fa
Otaku1120 <157336349+Otaku1120@users.noreply.github.com>
2024-02-04 02:38:46 +0000

    Shaping cirlce - global

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let circleSize= 0;
+
 function setup() {
   createCanvas(320, 240);
 }
@@ -8,11 +10,14 @@ function draw() {
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
+    fill(255,0,0);
+    circleSize= 150;
   }
   else { 
+  
     // otherwise do this:
-    
+    fill(0);
+    circleSize=100;
   }
-  ellipse(width/2, height/2, 100, 100);
+  ellipse(width/2, height/2, circleSize, circleSize);
 }
